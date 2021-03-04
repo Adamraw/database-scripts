1a146083e6de6e5f0e4c6c421b6904dfb88124f8
Adamraw <59123629+Adamraw@users.noreply.github.com>
2021-03-04 18:36:59 +0100

    Update DuplicateCheck.py

--- DuplicateCheck.py
+++ DuplicateCheck.py
@@ -14,8 +14,12 @@ args = parser.parse_args()
 fdf_scanner_path = Path('./duplicate_scripts/fdf_scanner.py')
 
 #define find_dups
-def find_dups(src_dir,trg_dir = '.\dup' ):
-    command = r'python {} -i {} -o {}'.format(fdf_scanner_path,src_dir,trg_dir)
+def find_dups(src_dir,trg_dir = '.\dup'):
+    command = r'python {} -i {} -o {} -w'.format(fdf_scanner_path,src_dir,trg_dir)
+    os.system(command)
+
+def find_dups_linux(src_dir,trg_dir = '.\dup'):
+    command = r'python {} -i {} -o {} '.format(fdf_scanner_path,src_dir,trg_dir)
     os.system(command)
 
 #define remove_ups, runs dup file, which will delete found duplicate files. Toggle for OS
@@ -23,7 +27,7 @@ def remove_dups():
     if platform.system() == 'Windows':
         command = r'dup.BAT'
     else:
-        command = r'dup.sh'
+        command = r'sh dup.sh'
     os.system(command)
 
 #main function that is called when script is run from the CLI, handles logic for which mode to run the function in
@@ -36,7 +40,7 @@ def main():
                 find_dups(r'{}'.format(args.input_directory))
                 remove_dups()
             else:
-                find_dups(r'{}'.format(args.input_directory),trg_dir='./dup')
+                find_dups_linux(r'{}'.format(args.input_directory),trg_dir='./dup')
                 remove_dups()
         #toggle for delete
         elif args.mode == 'd':
@@ -46,7 +50,7 @@ def main():
             if platform.system() == 'Windows':
                 find_dups(r'{}'.format(args.input_directory))
             else:
-                find_dups(r'{}'.format(args.input_directory), trg_dir='./dup')
+                find_dups_linux(r'{}'.format(args.input_directory), trg_dir='./dup')
 
     else:
         print('Invalid filepath - filepath does not exist. Please specify a valid filepath.')
